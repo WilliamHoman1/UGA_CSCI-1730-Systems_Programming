33d000729b64f05e007b1dbf1a3d33fece023e96
WilliamHoman1 <homando3@gmail.com>
2026-08-24 11:57:14 -0400

    First Systems Programming Assignment
    Practicing using Unix, Emacs, and C code in terminal. Using Odin, the UGA environment to develop.

---
+++ Lab01-UsingUnixAndTerminal/lab01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World!\n");
+  printf("I setup my odin account to use gcc and g++ version 11.2.0.\n");
+  printf("I practiced using basic Unix commands.\n");
+  return 0;
+}
